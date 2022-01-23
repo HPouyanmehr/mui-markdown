3f234e08221f1673f561b77f5c4afe99241fc451
Hosein Pouyanmehr <a.h.pouyanmehr@gmail.com>
2022-01-23 22:09:34 +0330

    Fix paragraph example in storybook

--- src/stories/Paragraph.stories.tsx
+++ src/stories/Paragraph.stories.tsx
@@ -3,7 +3,7 @@ import MuiMarkdown from '..';
 
 const stories = storiesOf('MuiMarkdown', module);
 
-const paragraph = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam quibusdam nobis ex recusandae incidunt, inventore neque optio, quis officiis reprehenderit, quisquam assumenda! Fugit doloribus, illum adipisci pariatur cumque obcaecati at?`;
+const paragraph = `<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam quibusdam nobis ex recusandae incidunt, inventore neque optio, quis officiis reprehenderit, quisquam assumenda! Fugit doloribus, illum adipisci pariatur cumque obcaecati at?</p>`;
 
 stories.add('Paragraph', () => {
   return <MuiMarkdown>{paragraph}</MuiMarkdown>;
