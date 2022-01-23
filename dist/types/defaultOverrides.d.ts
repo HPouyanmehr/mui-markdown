8acae9ab176ccebdc2d06ea1069a46e701b80b12
Hosein Pouyanmehr <85900067+hajhosein@users.noreply.github.com>
2022-01-23 15:42:05 +0330

    Delete dist directory

--- dist/types/defaultOverrides.d.ts
+++
@@ -1,3 +0,0 @@
-import { MarkdownToJSX } from 'markdown-to-jsx';
-declare const defaultOverrides: MarkdownToJSX.Overrides;
-export default defaultOverrides;
--- dist/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-import MuiMarkdown from './MuiMarkdown';
-export default MuiMarkdown;
-export type { MuiMarkdownProps as MuiMarkdownProps } from './MuiMarkdown';
-export declare const defaultOverrides: import("markdown-to-jsx").MarkdownToJSX.Overrides;
