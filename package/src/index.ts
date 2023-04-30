54a8bd6c51a291e0a9da47bc0d6f9d2982d5f0e9
Hosein Pouyanmehr <a.h.pouyanmehr@gmail.com>
2023-05-01 00:00:00 +0430

    fix: Remove unnecessary files

---
+++ package/src/index.ts
@@ -0,0 +1,10 @@
+// Types
+export type { Options, Overrides } from 'types/markdown';
+export type { MuiMarkdownProps } from 'types/muiMarkdown';
+export type { PrismTheme } from 'types/prism';
+
+// Utilities
+export { defaultOverrides, getOverrides } from 'overrides';
+
+// MUI Markdown
+export { default as MuiMarkdown } from 'MuiMarkdown';
