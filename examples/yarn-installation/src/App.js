55ab40f234e65273cc6570e3a9f20a02bb70a51a
Hosein Pouyanmehr <a.h.pouyanmehr@gmail.com>
2023-07-28 11:07:58 +0330

    feat: Add yarn-installation example

--- examples/yarn-installation/src/App.js
+++ examples/yarn-installation/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { MuiMarkdown } from 'mui-markdown';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const md = `
+# Heading One
 
+## Heading Two
+`;
+
+const App = () => (
+  <div className='App'>
+    <MuiMarkdown>{md}</MuiMarkdown>
+  </div>
+);
 export default App;
