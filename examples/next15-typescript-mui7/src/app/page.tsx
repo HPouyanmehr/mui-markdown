f085ca53a171645d76dbbbf325bc9439528f4d79
Hosein Pouyanmehr <a.h.pouyanmehr@gmail.com>
2025-03-30 22:16:38 +0330

    feat: Install mui7 in next15-typescript-mui7 example

--- examples/next15-typescript-mui7/src/app/page.tsx
+++ examples/next15-typescript-mui7/src/app/page.tsx
@@ -1,5 +1,7 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import { Button } from '@mui/material';
+
+import styles from './page.module.css';
 
 export default function Home() {
   return (
@@ -7,8 +9,8 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
+          src='/next.svg'
+          alt='Next.js logo'
           width={180}
           height={38}
           priority
@@ -23,67 +25,68 @@ export default function Home() {
         <div className={styles.ctas}>
           <a
             className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             <Image
               className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
+              src='/vercel.svg'
+              alt='Vercel logomark'
               width={20}
               height={20}
             />
             Deploy now
           </a>
           <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+            target='_blank'
+            rel='noopener noreferrer'
             className={styles.secondary}
           >
             Read our docs
           </a>
         </div>
+        <Button variant='contained'>Mui Button</Button>
       </main>
       <footer className={styles.footer}>
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <Image
             aria-hidden
-            src="/file.svg"
-            alt="File icon"
+            src='/file.svg'
+            alt='File icon'
             width={16}
             height={16}
           />
           Learn
         </a>
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <Image
             aria-hidden
-            src="/window.svg"
-            alt="Window icon"
+            src='/window.svg'
+            alt='Window icon'
             width={16}
             height={16}
           />
           Examples
         </a>
         <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <Image
             aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
+            src='/globe.svg'
+            alt='Globe icon'
             width={16}
             height={16}
           />
